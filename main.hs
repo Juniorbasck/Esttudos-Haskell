71e16813d13450bbe4b3a997f1167c154c44304b
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-01-12 20:59:05 -0300

    atualizacoes

---
+++ main.hs
@@ -0,0 +1,17 @@
+import Distribution.Fields.LexerMonad (LexState)
+
+data Lista a = Nulo | a :>: Lista a deriving Show
+
+listaX :: Lista Int
+listaX = 10 :>: (20 :>: ( 30 :>: Nulo))
+
+
+verificaElemento :: Int -> Lista Int -> Bool
+verificaElemento _ Nulo = False
+verificaElemento elementoD (elementoL :>: restoLista)
+    | elementoL == elementoD = True
+    | otherwise = verificaElemento elementoD restoLista
+
+inserirElemento :: Int -> Lista Int -> Lista Int
+inserirElemento :: int -> Lista Int -> Lista Int 
+inserirElemento novoElemento Nulo = novoElemento :>:
